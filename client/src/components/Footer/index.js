3fc3bcf09a1daacb8394db9b4e3ea9f07209ea95
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-10-29 18:34:25 -0400

    navbar and footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // Import hooks from React Router
 import { useLocation, useNavigate } from 'react-router-dom';
+import { Box } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 
 const Footer = () => {
   // We retrieve the current `location` object data from React Router
@@ -8,19 +10,13 @@ const Footer = () => {
   // We get React Router's history object so we can access and adjust browser history
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto text-dark p-4">
-      <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            // Go back to the previous page in our browser's history
-            onClick={() => navigate(-1)}
-          >
-            &larr; Go Back
-          </button>
-        )}
-        <h4>&copy; {new Date().getFullYear()} - CompLogic, LLC. All Rights Reserved.</h4>
-      </div>
+    <footer class="page-footer font-small cyan darken-3">
+      <Box sx={{ textAlign: 'center', fontWeight: 'regular', fontFamily: 'default' }}>
+        <Typography variant="caption" component="div" sx={{ flexGrow: 1 }}>
+          &copy; {new Date().getFullYear()} - CompLogic, LLC. All Rights Reserved.
+        </Typography>
+      </Box>
+
     </footer>
   );
 };
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,11 +3,33 @@ import { AppBar } from "@material-ui/core";
 import { Box } from "@material-ui/core";
 import { Toolbar } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
-import { Button }from "@material-ui/core";
-import { IconButton }from "@material-ui/core";
+import Button from '@mui/material/Button';
+import { IconButton } from "@material-ui/core";
 import { Menu as MenuIcon } from "@material-ui/icons";
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import SupportIcon from '@mui/icons-material/Support';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
+const style = {
+  m: 2,
+  color: 'white',
+  borderRadius: 15,
+};
+
 
 function Navbar() {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -19,15 +41,38 @@ function Navbar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <MenuIcon id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick} />
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>About Us</MenuItem>
+              <MenuItem onClick={handleClose}>Services</MenuItem>
+              <MenuItem onClick={handleClose}>News/Media</MenuItem>
+            </Menu>
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            CompLogic
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Button startIcon={<AlternateEmailIcon />} sx={{ ...style }} variant="contained" size="small">email@sample.com</Button>
+            <Button startIcon={<LocalPhoneIcon />} sx={{ ...style }} variant="contained" size="small">786-483-6751</Button>
+            <Button startIcon={<SupportIcon />} sx={{ ...style }} variant="contained" size="small">Submit Ticket</Button>
+          </Box>
+
         </Toolbar>
       </AppBar>
     </Box>
+
   )
 };
 export default Navbar;
