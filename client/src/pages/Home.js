48b6bbdaad4b9dad669ffb1ccf61454d7cadee6d
Anastasia <anastas.tarasenko001@mymdc.net>
2022-11-04 17:20:19 -0400

    main page cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import { Box } from "@material-ui/core";
+import Button from '@mui/material/Button';
 import AnimatedCarousel from '../components/Carousel';
 import ActionAreaCard from '../components/Cards';
+import { StayPrimaryLandscape } from '@material-ui/icons';
+
+const style = {
+  width: 250,
+  height: 250,
+  margin: 45,
+  bgcolor: "rgba(77,77,77,0.62)"
+  // display: 'flex',
+  // justifyContent: 'center'
+};
+
+const bstyle = {
+  my: 12.8,
+  borderRadius: 0,
+}
 
 const Home = () => {
   return (
     <React.Fragment>
-      <AnimatedCarousel/>
-      <Box sx={{ display: 'flex', justifyContent: 'center', pb: 6  }}>
-        <ActionAreaCard sx={{ m: 1 }}/>
-        <ActionAreaCard sx={{ m: 0.5 }}/>
-        <ActionAreaCard sx={{ m: 0.5 }}/>
+      <AnimatedCarousel />
+      <Box sx={{ display: 'flex', justifyContent: 'center', pb: 6 }}>
+        <Box textAlign='center' sx={{ ...style }}>
+          <Button
+          sx={{ ...bstyle }}
+            color="secondary"
+            variant="contained"
+          >
+            Learn More
+          </Button>
+        </Box>
+        <Box textAlign='center' sx={{ ...style }}>
+          <Button
+          sx={{ ...bstyle }}
+            color="secondary"
+            variant="contained"
+          >
+            Learn More
+          </Button>
+        </Box>
+        <Box textAlign='center' sx={{ ...style }}>
+          <Button
+          sx={{ ...bstyle }}
+            color="secondary"
+            variant="contained"
+          >
+            Learn More
+          </Button>
+        </Box>
+        <Box textAlign='center' sx={{ ...style }}>
+          <Button
+          sx={{ ...bstyle }}
+            color="secondary"
+            variant="contained"
+          >
+            Learn More
+          </Button>
+        </Box>
+
       </Box>
     </React.Fragment>
   );
