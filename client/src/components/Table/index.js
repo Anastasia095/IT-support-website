1ee0d56bfdcfa35d4b18f5917e9a2a9b3546513b
Anastasia <anastas.tarasenko001@mymdc.net>
2022-11-28 15:41:22 -0500

    fixed props

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -7,49 +7,48 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
 
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
 
 export default function BasicTable(props) {
-  console.log("test");
-  console.log(props.ticket);
-  return (
-    <TableContainer component={Paper} sx={{ maxWidth: '97%', m: 'auto'  }}>
-      <Table sx={{ minWidth: 300 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Ticket ID</TableCell>
-            <TableCell align="right">Subject</TableCell>
-            <TableCell align="right">Status</TableCell>
-            <TableCell align="right">Last Update</TableCell>
-            <TableCell align="right">Assigned</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.ticket.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.title}</TableCell>
-              <TableCell align="right">{row.status}</TableCell>
-              <TableCell align="right">{row.updatedAt}</TableCell>
+
+  const data = props.ticket;
+  if (data.length === 0) {
+    console.log("length < 0");
+    return (
+      <h3>No Tickets Yet</h3>
+    );
+  };
+
+  // console.log(data.length);
+
+    return (
+      <TableContainer component={Paper} sx={{ maxWidth: '97%', m: 'auto' }}>
+        <Table sx={{ minWidth: 300 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Ticket ID</TableCell>
+              <TableCell align="right">Subject</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Last Update</TableCell>
+              <TableCell align="right">Assigned</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.title}</TableCell>
+                <TableCell align="right">{row.status}</TableCell>
+                <TableCell align="right">{row.updatedAt}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
